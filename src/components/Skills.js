8e84277ec1e53d1afd3d25b3fb3708bc5ad69b6a
Ihtisham Khattak <ikpashteen1@gmail.com>
2023-03-04 17:40:30 +0500

    circular progress bar done

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,16 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+// import meter1 from "../assets/img/meter1.svg";
+// import meter2 from "../assets/img/meter2.svg";
+// import meter3 from "../assets/img/meter3.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 export const Skills = () => {
-  const percentage = 66;
+  const content_writting = 76;
+  const blog_writing = 85;
+  const proof_reading = 90;
+  const short_stories_writting = 85;
+
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -45,22 +49,63 @@ export const Skills = () => {
                 className="owl-carousel owl-theme skill-slider"
               >
                 <div className="item">
-                  <img src={meter1} alt="proof" />
+                  <CircularProgressbar
+                    className="circular__progress"
+                    value={proof_reading}
+                    text={`${proof_reading}%`}
+                    styles={buildStyles({
+                      rotation: 0.25,
+                      textSize: "14px",
+                      pathTransitionDuration: 0.1,
+                      pathColor: `rgb(251, 228, 121, ${proof_reading / 100})`,
+                      textColor: "#ffffff",
+                    })}
+                  />
                   <h5>Proof Reading</h5>
                 </div>
                 <div className="item">
                   <CircularProgressbar
-                    value={percentage}
-                    text={`${percentage}%`}
+                   className="circular__progress"
+                    value={content_writting}
+                    text={`${content_writting}%`}
+                    styles={buildStyles({
+                      rotation: 0.25,
+                      textSize: "14px",
+                      pathTransitionDuration: 0.1,
+                      pathColor: `rgb(251, 228, 121, ${proof_reading / 100})`,
+                      textColor: "#ffffff",
+                    })}
                   />
                   <h5>Content Writing</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="blog" />
+                  <CircularProgressbar
+                   className="circular__progress"
+                    value={blog_writing}
+                    text={`${blog_writing}%`}
+                    styles={buildStyles({
+                      rotation: 0.25,
+                      textSize: "14px",
+                      pathTransitionDuration: 0.1,
+                      pathColor: `rgb(251, 228, 121, ${proof_reading / 100})`,
+                      textColor: "#ffffff",
+                    })}
+                  />
                   <h5>Blog Writing</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="stories" />
+                  <CircularProgressbar
+                   className="circular__progress"
+                    value={short_stories_writting}
+                    text={`${short_stories_writting}%`}
+                    styles={buildStyles({
+                      rotation: 0.25,
+                      textSize: "14px",
+                      pathTransitionDuration: 0.1,
+                      pathColor: `rgb(251, 228, 121, ${proof_reading / 100})`,
+                      textColor: "#ffffff",
+                    })}
+                  />
                   <h5>Short Stories</h5>
                 </div>
               </Carousel>
